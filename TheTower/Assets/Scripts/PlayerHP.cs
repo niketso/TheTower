bc7d908443415a73f61b32cdc66ba0698e616bdc
Unknown <nicolaspiccitto@gmail.com>
2018-08-30 17:50:00 -0300

    TheTower-v0.3
    Add Script
    -PlayerHP.cpp

---
+++ TheTower/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour {
+
+    private float playerHP = 1;
+    [SerializeField]
+    private float playerChances;
+
+    private void Update()
+    {
+        if (playerHP <= 0) 
+        {
+            //crear un enemigo en esta  posicion
+        }
+    }
+    private void TakeEnemyDamage(float damage) 
+    {
+        playerHP -= damage;
+    }
+}
