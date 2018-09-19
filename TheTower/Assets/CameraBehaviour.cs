8cf510de6c6ac812c2f2de4ec1d5372e880cace0
Unknown <agustingarcia1997@gmail.com>
2018-09-19 01:52:50 -0300

    TheTower-v0.14
    La camara tiene limites a ambos lados del mundo, el jugador aun puede salirse de los limites

---
+++ TheTower/Assets/CameraBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    [SerializeField] Transform leftLimit;
+    [SerializeField] Transform rightLimit;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+    void Update ()
+    {
+        Vector3 rightPos = cam.ViewportToWorldPoint(Vector3.one);
+        Vector3 leftPos = cam.ViewportToWorldPoint(Vector3.zero);
+
+        checkbounds(rightPos, leftPos);
+	}
+
+    void checkbounds(Vector3 right, Vector3 left)
+    {
+        if (right.x >= rightLimit.position.x)
+            cam.transform.position = cam.transform.position - new Vector3(right.x - rightLimit.position.x, 0, 0);
+        if (left.x <= leftLimit.position.x)
+            cam.transform.position = cam.transform.position + new Vector3(leftLimit.position.x - left.x, 0, 0);
+    }
+}
