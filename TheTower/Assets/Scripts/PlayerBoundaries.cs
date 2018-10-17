d7e3bb6eaed06f58aba3ba3972e574e14ed5d2a8
Unknown <agustingarcia1997@gmail.com>
2018-10-16 23:11:37 -0300

    script PlayerBoundaries agregado para caminata jugador
    El jugador ahora se bloquea al intentar caminar fuera del mapa, es posible salir si se usa el dash

---
+++ TheTower/Assets/Scripts/PlayerBoundaries.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoundaries : MonoBehaviour {
+
+    [SerializeField] Vector3 pushDistance;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.position -= pushDistance;
+    }
+}
