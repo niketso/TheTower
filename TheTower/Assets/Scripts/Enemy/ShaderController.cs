c26f950bf6a2c81be6f6af4b9aae56b5fb649ed8
Lithiot <agustingarcia1997@gmail.com>
2020-07-19 18:42:04 -0300

    Added URP and dissolve shader for spawning entities

---
+++ TheTower/Assets/Scripts/Enemy/ShaderController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    public float maxValue = 0;
+    [Range(0, 1)]
+    public float spawnEffectSpeed;
+    public Color effectColor;
+
+    private Material shader;
+
+    private void Awake()
+    {
+        shader = GetComponent<SpriteRenderer>().material;
+
+        shader.SetColor("_EdgeColor" , effectColor);
+
+        TriggerSpawn();
+    }
+
+    public void TriggerSpawn() 
+    {
+        StartCoroutine(SpawnEffect());
+    }
+
+    IEnumerator SpawnEffect() 
+    {
+        for (float i = 0; i < maxValue; i += spawnEffectSpeed) 
+        {
+            shader.SetFloat("_Fade" , i);
+            yield return null;
+        }
+    }
+}
