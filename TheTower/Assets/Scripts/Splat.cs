0878992c397d1fa5500f8181286618d97c4b5fd7
Lithiot <agustingarcia1997@gmail.com>
2020-07-31 21:15:18 -0300

    Added new animations for blocker, and splat spawning at enemy death pos
    Also fixed the error where the ranged enemies would shot at the wrong side of the map and completely miss the player

---
+++ TheTower/Assets/Scripts/Splat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splat : MonoBehaviour, iPoolable
+{
+    public Sprite[] splats;
+
+    public float minSize;
+    public float maxSize;
+
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void OnPool()
+    {
+    }
+
+    public void OnUnpool()
+    {
+        sprite.sprite = splats[Random.Range(0 , splats.Length)];
+
+        float randomRot = Random.Range(0.0f, 360.0f);
+
+        Vector3 randomSize = new Vector3(Random.Range(minSize, maxSize), Random.Range(minSize, maxSize), 1.0f);
+
+        transform.rotation = Quaternion.Euler(0.0f , 0.0f , randomRot);
+        transform.localScale = randomSize;
+    }
+}
